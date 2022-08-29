315948979cae853b77e9399c412641c075c42629
Sharkgrammer <gillskk@gmail.com>
2022-08-29 19:13:56 +0100

    Search Redesign

--- src/components/search/SearchBox.vue
+++ src/components/search/SearchBox.vue
@@ -9,57 +9,77 @@
   </div>
 
 
-  <div class="text-gray-100 border-gray-300 border hover:border-accent rounded-xl mt-2 p-2 mr-2">
+  <div class="text-gray-100 mt-2 mr-2">
 
-    <div v-if="searchResults" :key="searchResults">
+    <div v-if="userResults && postResults" :key="userResults && postResults">
 
-      <div v-if="searchResults.length > 0">
+      <!-- Users -->
+      <div v-if="userResults" :key="userResults" class="border-gray-300 border hover:border-accent rounded-xl p-2 mb-3">
 
-        <div v-for="(result, index) in searchResults" :key="result">
+        <div v-if="userResults.length > 0">
 
-          <!-- Extra Search Things -->
-          <div v-if="index === 0 && userTotal !== 0" class="-mb-2">
-            <p class="text-2xl text-center">{{ 'People with \'' + searchQuery + '\'' }}</p>
+          <div class="-mb-1">
+            <p class="text-2xl text-center">People with '<span class="text-accent">{{ searchQuery }}</span>'</p>
             <HRV2SM class="m-5 mb-0 mt-0"/>
           </div>
 
-          <div v-if="index === userTotal && postTotal !== 0" :key="userTotal" class="mb-3 mt-3">
-            <p class="text-2xl text-center">{{ 'Posts with \'' + searchQuery + '\'' }}</p>
+          <div v-for="user in userResults" :key="user">
+
+            <div>
+              <FollowUser :user="user" :search="searchQuery" @followUpdate="this.$emit('followUpdate')"/>
+            </div>
+
+          </div>
+
+          <div class="p-1 text-gray-500 text-center cursor-pointer hover:text-gray-400 mt-2 mb-2 select-none">
+            <p @click="loadMoreUsers" v-if="userResults.length >= userAmt" :key="userAmt">Load More Users...</p>
+            <p v-else>No More People Found</p>
+          </div>
+        </div>
+
+        <div v-else>
+          <p class="p-1 text-gray-500 text-center cursor-pointer">No people found :( </p>
+        </div>
+
+      </div>
+
+      <!-- Posts -->
+      <div v-if="postResults" :key="postResults" class="border-gray-300 border hover:border-accent rounded-xl p-2">
+
+        <div v-if="postResults.length > 0">
+
+          <div class="mb-3">
+            <p class="text-2xl text-center">Posts with '<span class="text-accent">{{ searchQuery }}</span>'</p>
             <HRV2SM class="m-5 mb-0 mt-0"/>
           </div>
 
+          <div v-for="post in postResults" :key="post">
 
-          <!-- Post Design -->
-          <div v-if="result.pid">
-            <SmallUserPost :post="result" :search="searchQuery" @followUpdate="this.$emit('followUpdate')"/>
+            <div>
+              <SmallUserPost :post="post" :search="searchQuery" @followUpdate="this.$emit('followUpdate')"/>
+            </div>
+
           </div>
 
-
-          <!-- User Design -->
-          <div v-else>
-            <FollowUser :user="result" :search="searchQuery" @followUpdate="this.$emit('followUpdate')"/>
+          <div class="p-1 text-gray-500 text-center cursor-pointer hover:text-gray-400 mb-2 select-none">
+            <p @click="loadMorePosts" v-if="postResults.length >= postAmt" :key="postAmt">Load More Posts...</p>
+            <p v-else>No More Posts Found</p>
           </div>
 
-
-          <!-- Extra Search Things -->
-          <p class="p-1 text-gray-500 text-center cursor-pointer hover:text-gray-400 mb-5 mt-2" @click="loadMoreUsers"
-             v-if="index === userTotal - 1 && userTotal >= userAmt">Load More Users...</p>
-
-          <p class="p-1 text-gray-500 text-center cursor-pointer hover:text-gray-400 mb-2 mt-2" @click="loadMorePosts"
-             v-if="index === searchResults.length - 1 && postTotal >= postAmt">Load More Posts...</p>
 
         </div>
+
+        <div v-else>
+          <p class="p-1 text-gray-500 text-center cursor-pointer">No Posts found :( </p>
+        </div>
+
       </div>
 
-
-      <div v-else>
-        <p class="p-1 text-gray-500 text-center cursor-pointer mb-2 mt-2">No people or posts found :( </p>
-      </div>
 
     </div>
 
-    <div v-else>
-      <p class="p-1 text-gray-500 text-center cursor-pointer mb-2 mt-2">Nothing here yet</p>
+    <div v-else class="border-gray-300 border rounded-xl">
+      <p class="p-1 text-gray-500 text-center cursor-pointer mb-2 mt-2">Nothing here yet.</p>
     </div>
 
   </div>
@@ -80,11 +100,10 @@ export default {
   data() {
     return {
       searchQuery: null,
-      searchResults: null,
+      userResults: null,
+      postResults: null,
       userAmt: 3,
-      userTotal: 3,
       postAmt: 3,
-      postTotal: 3
     }
   },
   methods: {
@@ -113,19 +132,21 @@ export default {
         if (data !== false) {
           // Best to compute how much data we have :( we get back both posts and users
           // posts have a pid key, users don't
-          this.postTotal = 0;
-          this.userTotal = 0;
+          this.postResults = []
+          this.userResults = []
+
           for (let obj of data) {
-            obj.pid !== undefined ? this.postTotal++ : this.userTotal++;
+            obj.pid !== undefined ? this.postResults.push(obj) : this.userResults.push(obj);
           }
 
-          this.searchResults = data;
         } else {
-          this.searchResults = [];
+          this.postResults = []
+          this.userResults = []
         }
 
       } else {
-        this.searchResults = null
+        this.postResults = null
+        this.userResults = null
       }
     }
   },
