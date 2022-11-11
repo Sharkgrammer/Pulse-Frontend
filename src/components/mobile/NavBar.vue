64270f3b7b090913bc261b7923476f535fcf8898
Sharkgrammer <gillskk@gmail.com>
2022-11-11 01:30:36 +0000

    Updated mobile nav

--- src/components/mobile/NavBar.vue
+++ src/components/mobile/NavBar.vue
@@ -2,17 +2,18 @@
 
   <div v-if="showSystem || showFriend" class="flex justify-center pb-5 border-b-0 border border-gray-700">
     <div v-if="showSystem" class="md:w-profile">
-      <SystemTab />
+      <SystemTab/>
     </div>
 
     <div v-if="showFriend" class="md:w-friend">
-      <FriendTab />
+      <FriendTab/>
     </div>
   </div>
 
-  <div class="grid grid-cols-2 gap-0 text-center flex items-center text-gray-700 dark:text-gray-300 font-bold text-lg">
-    <p class="border border border-gray-700 p-3" @click="showSystemTab">Your Options</p>
-    <p class="border border-l-0 border-gray-700 p-3" @click="showFriendTab">Your Profile</p>
+  <div class="grid grid-cols-2 gap-0 text-center flex items-center text-gray-700 dark:text-gray-300 font-bold text-lg
+        border-b dark:border-gray-700 border-gray-400">
+    <p class="border-r dark:border-gray-700 border-gray-400 p-3" @click="showSystemTab">Your Options</p>
+    <p class="p-3" @click="showFriendTab">Your Profile</p>
   </div>
 
 </template>
@@ -20,6 +21,7 @@
 <script>
 import SystemTab from "@/components/system/SystemTab";
 import FriendTab from "@/components/system/FriendTab";
+
 export default {
   name: "NavBar",
   components: {FriendTab, SystemTab},
