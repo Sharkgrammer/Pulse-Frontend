56c1ea7f548abaf9e7dc6d5d11a5d73f52e0877b
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-26 22:33:10 +0100

    Build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const webpack = require('webpack');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/Pulse-Frontend/" : "/",
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
+  },
+});
