a77dc199188df57884f6a88c4b64d22d9e2df12c
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-26 22:39:36 +0100

    Updated forgotten router code

--- src/router/router.js
+++ src/router/router.js
@@ -41,7 +41,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(process.env.NODE_ENV === "production" ? "/Pulse-Frontend/" : "/"),
     routes,
 });
 
